95fda0b0d02d3a06dd265e9b5979bb1eca4bffda
Aries-Indrabayu <indra13498@gmail.com>
2025-08-20 19:17:36 +0700

    Latihan 1-6

--- latihan-003-A.js
+++ latihan-003-A.js
@@ -1,17 +1,45 @@
-// operator Precedence untuk perkalian
-let hasil = 3 + 4 * 2;
-console.log(hasil);
-// operator Precedence untuk ()
-hasil = (3 + 4) * 2;
-console.log(hasil);
+let a = 9;
+let b = 4;
+let hasil;
 
-// operator Precedence untuk ++, --
-let x = 5,
-  y = 4;
+// 1. Operator penambahan +
+hasil = a + b;
+console.log("a + b = ", a + b);
+console.log("a + b = ", hasil);
+console.log("a + b = ${hasil}");
 
-hasil = 3 - 2 + ++x;
-console.log(hasil);
+// ` = backtick, " = kutif 2, ' = kutif 1
 
-// eksponensial
-hasil = 2 * 4 ** 2;
-console.log(hasil);
+console.log(`a + b = ${hasil}`);
+
+// 2. Operator pengurangan -
+hasil = a - b;
+console.log(`a - b = ${hasil}`);
+
+// 3. Operator pembagian /
+hasil = a / b;
+console.log(`a / b = ${hasil}`);
+
+// 4. Operator perkalian *
+hasil = a * b;
+console.log(`a * b = ${hasil}`);
+
+// 5. Operator modulus/modulo %
+hasil = a % b;
+console.log(`a % b = ${hasil}`);
+
+// 6. Operator Pangkat/ Eksponensial **
+let x = 4;
+let y = 2;
+hasil = 4 ** 2;
+console.log(`a pangkat/eksponensial b = ${hasil}`);
+
+// 7. Operator Increment ++ => ditambah 1
+x++;
+hasil = x;
+console.log(`Increment dari nilai x = ${hasil}`);
+
+// 8. Operator Decrement -- => dikurang 1
+y--;
+hasil = y;
+console.log(`Decrement dari nilai y = ${hasil}`);
--- latihan-003-B.js
+++ latihan-003-B.js
@@ -1,46 +1,17 @@
-// AND = &&
-/*
-Kondisi-1 && Kondisi-2
-7 > 3 && 7 < 9
-T && T = T
-AND jika Kondisi Kiri dan Kondisi Kanan True maka hasilnya True selain itu Flase
+// operator Precedence untuk perkalian
+let hasil = 3 + 4 * 2;
+console.log(hasil);
+// operator Precedence untuk ()
+hasil = (3 + 4) * 2;
+console.log(hasil);
 
-Konddisi 1      vs      Kondisi 2       AND
-    TRUE                   TRUE         TRUE
-    FALSE                  TRUE         FALSE
-    TRUE                  FALSE         FALSE
-    FALSE                 FALSE         FALSE
+// operator Precedence untuk ++, --
+let x = 5,
+  y = 4;
 
-Konddisi 1      vs      Kondisi 2       OR
-    TRUE                   TRUE         TRUE
-    FALSE                  TRUE         TRUE
-    TRUE                  FALSE         TRUE
-    FALSE                 FALSE         FALSE
+hasil = 3 - 2 + ++x;
+console.log(hasil);
 
-Konddisi        NOT
-    TRUE        FLASE
-    FLASE        TRUE
-*/
-// OR = ||
-// NOT = !
-
-let nilai = 85;
-
-// AND
-console.log("Operator logika AND");
-console.log(nilai > 75 && nilai < 90);
-console.log(nilai < 60 && nilai > 80);
-console.log(nilai > 60 && nilai == 90);
-console.log(nilai == 60 && nilai == 90);
-
-// OR
-console.log("Operator logika OR");
-console.log(nilai > 75 || nilai < 90);
-console.log(nilai < 60 || nilai > 80);
-console.log(nilai > 60 || nilai == 90);
-console.log(nilai == 60 || nilai == 90);
-
-// NOT
-console.log("Operator logika NOT");
-console.log(!nilai > 75);
-console.log(!nilai < 75);
+// eksponensial
+hasil = 2 * 4 ** 2;
+console.log(hasil);
