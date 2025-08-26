a2d0acc9ac723a2491c9f79cc4246cb65cb6bc97
Aries-Indrabayu <indra13498@gmail.com>
2025-08-26 18:58:58 +0700

    P7

---
+++ tes.php
@@ -0,0 +1,19 @@
+<?php
+$angka = 5;
+$nama = "Bayu"
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <button onclick="notif();" style="background-color: red;">Notifikasi</button>
+    <script src="latihan-007.js"></script>
+</body>
+
+</html>
